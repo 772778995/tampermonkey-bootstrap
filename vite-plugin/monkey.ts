a98c7dab79536efb4f084496536b7f2a83975aef
whc <772778995@qq.com>
2023-11-27 12:24:38 +0800

    chore: 替换CDN源

--- vite-plugin/monkey.ts
+++ vite-plugin/monkey.ts
@@ -1,7 +1,7 @@
-import monkey from 'vite-plugin-monkey'
+import monkey, { cdn } from 'vite-plugin-monkey'
 import packageInfo from '../package.json'
 
-export default () =>
+export default async () =>
   monkey({
     entry: 'src/main.tsx',
     userscript: {
@@ -9,5 +9,16 @@ export default () =>
       icon: 'https://img.wuhaochao.top/funny.png',
       namespace: 'npm/vite-plugin-monkey',
       match: ['http://127.0.0.1:54088/abc/editor.html*']
+    },
+    build: {
+      externalGlobals: {
+        react: cdn.bootcdn('React', 'umd/react.production.min.js'),
+        'react-dom/client': cdn.bootcdn(
+          'ReactDOM',
+          'umd/react-dom.production.min.js'
+        ),
+        jquery: cdn.bootcdn('jQuery', 'jquery.min.js'),
+        lodash: cdn.bootcdn('_', 'lodash.min.js')
+      }
     }
   })
