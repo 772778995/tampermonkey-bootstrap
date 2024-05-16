defeecbf0afd767fb76ec8d33ff175251edf13c0
whc <772778995@qq.com>
2024-05-16 10:21:15 +0800

    test: 测试线上路径

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,13 @@ import $ from 'jquery'
 import packageJSON from '../package.json'
 import 'dayjs'
 import { win } from './utils/tampermonkey'
+import axios from 'axios'
+
+axios
+  .get('https://github.com/772778995/tampermonkey-bootstrap/raw/socre/dist/score-editor-devtool.user.js')
+  .then((res) => {
+    console.error(res)
+  })
 
 export const rootElTag = `${packageJSON.name}`
 
